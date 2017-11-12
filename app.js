7e04d4969c802418905abfca0b489513af60b28e
vanasnip <ivan.mamenemuno@gmail.com>
2017-11-12 14:25:07 +0000

    04b notes on scope chain

--- app.js
+++ app.js
@@ -1,35 +1,19 @@
-//Scope chain
+//Scope chain b
 
-// execution context has access to outer environment
-// except global execution context
 
-// In a discussion about lexical environment
-// Q: where does function b() sit lexically?
-function b() { //A: on top of the global environment
+function b() { //Sitting on top of the global environment in the stack
     console.log(myVar); // 1
-}
-
-// where functions are concerned
-// order of invokation is what is important
-// order of declaration in the same execution
-// context has no baring on anything,
-// except its good practice to place dependancies 
-// above where they are a called.
-
-// in the case of b() the variable myVar isnt found in
-// its execution context in its scope,
-// so JS searches in the next immediate external
-// environment for a definition of myVar, if its not 
-// it will continue until it reaches the global
-// execution context, where it would be an error if
-// its never declared, or undefined if hoisted or 
-// if its declaired and assigned a value then 
-// that value will be returned
+}// attachted to the global object
 
 function a() {
+    function c() {//Sitting on top of the a() envitonment
+        console.log(myVar);
+    }
     var myVar = 2;
-    b();
+    b();// 1
+    c();// 2
 }
 
 var myVar = 1;
 a();
+//c(); // not defined error
