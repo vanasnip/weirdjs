23c5a992b1d840120e2e2f8ac033ff9f592469f8
vanasnip <ivan.mamenemuno@gmail.com>
2017-11-13 20:45:32 +0000

    05 notes on event queue

--- app.js
+++ app.js
@@ -1,19 +1,38 @@
-//Scope chain b
+// event queue
 
+// JS event queue has is list of notifications
+// of events
 
-function b() { //Sitting on top of the global environment in the stack
-    console.log(myVar); // 1
-}// attachted to the global object
+// The event queue get looked at when the
+// execusion stack is empty
 
-function a() {
-    function c() {//Sitting on top of the a() envitonment
-        console.log(myVar);
-    }
-    var myVar = 2;
-    b();// 1
-    c();// 2
+// once empty JS waits until an event is triggered
+// A click event for instance.
+// at this point this execution stack as stated
+// is empty.
+//  when a click event does occur then a new
+//execution context is generated becuase in
+// this example that click event had a function
+// as an event handler so a new execution context 
+// gets on the stack.
+
+// in the stricted terms javascript only runs 
+//synchronously. The browser puts supposed "async"
+// events into the event queue.
+
+// long running function
+function waitThreeSeconds(){
+    var ms = 3000 + new Date().getTime();
+    while(new Date() < ms){}
+    console.log('finished function');
 }
 
-var myVar = 1;
-a();
-//c(); // not defined error
+function clickHandler(){
+    console.log('click event');
+}
+
+//listen for the click event 
+document.addEventListener('click', clickHandler);
+
+waitThreeSeconds();
+console.log('finished execution');
