f3fd6d64d7a980116a0d5489ce62c0964957f127
vanasnip <ivan.mamenemuno@gmail.com>
2017-11-13 20:59:42 +0000

    07 notes on primitive types

--- app.js
+++ app.js
@@ -2,3 +2,20 @@
 
 // Is a type of data that respresents 
 //a single value that is not an object!
+
+//Undefined : represents lack of existance,
+// coder should never set a variable to "undefined"
+// but it certainly can be tested for.
+
+// Null : represents lack of existance
+// coder can set a variable as null.
+
+//boolean : true or false
+
+// number : Only one type of numeric respresentation
+// in javascript which is a Floating point number, 
+// this means theres always a decimal point attached
+
+// string : squence of characters
+
+// symbol : used in es6 ...to be continued
