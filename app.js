4155a2c4a1c530b627cadc869350bfef6169666a
vanasnip <ivan.mamenemuno@gmail.com>
2017-11-14 18:56:49 +0000

    12 Notes on Comparison operators

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
-// Coercion
-// converting a value from one type to another
+// Comparison operators
+console.log(1 < 2 < 3); // evaluates to true | expected
+console.log(3 < 2 < 1); // evaluates to true | unexpected
 
-// happens quite often in JS because its 
-// dynamically typed
+// the 2nd evals to true is left associativity
+console.log(3 < 2); // evals to false, .then
+console.log(false < 1); // js will try to coerce false to a number
+var a = Number(false);// evals to 0
+console.log(a); 
 
-var a = 1 + 2;
-console.log(a);
+//false < 1 eval to true bcoz == 0 < 1
 
-var b = 'hello ' + 'world';
-console.log(b);
+// more coercion
+Number('3'); // 3
+Number(undefined); // NaN - not a number
+Number(null); // 0
+Number('s'); // NaN
 
-var c = '1' + 2; 
-console.log(c); // output : 12
-//this means the value 2 was coerced into a string
+var b =  3 == 3; // true
+var c = '3' == 3; // true : coercion
+var d = '3' === 3; // false
+
+var e = false == 0; // true
+var f = null == 0; // false 
+//exception cose Number(null) = 0
+var g = null < 1; // true
+
+var h = "" == 0; // true
+// that means if you have
+var i = "" == false; // evals to true
+
+var j = '3' === 3; // false! tripple equals for the win
+
