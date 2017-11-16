d0292b428a5195c60054aa7aa8797160c6845c44
vanasnip <ivan.mamenemuno@gmail.com>
2017-11-16 06:36:53 +0000

    framework aside

--- app.js
+++ app.js
@@ -1,19 +1,6 @@
-// Default values
+// Framesworks aside
 
-function greet(name){
-    name = name || '<Your name here>';
-    // here JS will return the first value that is coercable
-    // to true. if the first value is undefined 
-    // then the second value will be returned
+console.log(libraryName);
 
-    // this also has an impact on 
-    // 0 || 1 returns 1
-    // undefined || 'hello' returns hello
-    // null || 'hello' returns hello
-    // "" || "hello" returns hello
-    console.log('Hello ' + name);
-}
-// calling it without a parameter
-greet('ivan');
-greet();//this wont throw error, hello undefined
-
+// see lib2.js
+// & lib1.js
