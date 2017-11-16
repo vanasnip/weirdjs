c557183a853dbe15eed66f9bb4382682bed48a98
vanasnip <ivan.mamenemuno@gmail.com>
2017-11-16 06:52:28 +0000

    notes on object dot notation

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
-// Objects and bracket notation | computed member access
+// Objects and dot notation
 
 var person = new Object();
 
 person['firstname'] = 'Ivan';
 person['lastname'] = 'Mams';
 
-var firstNameProperty = 'firstname';
+console.log(person.firstname);
+console.log(person.lastname);
 
-console.log(person);
-console.log(person[firstNameProperty]);
+person.address = new Object();
+person.address.street = "111 Main St.";
+person.address.city = 'New york';
+person.address.state = 'NY';
+
+console.log(person.address.street);
+console.log(person.address.city);
+console.log(person['address']['state']);
