fd6968677f62007c0550ddcbd795330414186865
vanasnip <ivan.mamenemuno@gmail.com>
2017-11-25 14:25:53 +0000

    Function are objects

--- app.js
+++ app.js
@@ -1,24 +1,2 @@
-// Objects and Json are different;
-//json doesnt allow functions as values
-var objectLiteral = {
-    firstname: 'Ivan',
-    isAProgrammer: true
-}
-
-
-var myJSON = { // stricter rules
-    "firstname": "Mary", //property names have to be in ""
-    "isAProgrammer": true
-}
-
-// all json is valid object literal notation
-// bit not all object literal notation is valid json
-// gotta have those property names in quotes!
-
-// because of json's popularity JS comes with json bells
-
-var OLNtoJSON = JSON.stringify(objectLiteral);
-console.log(OLNtoJSON);
-
-var JSONtoOLN = JSON.parse('{"firstname": "Mary", "isAProgrammer": true}');
-console.log(JSONtoOLN);
+// Functions are Objects
+// first class functions 
