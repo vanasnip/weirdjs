a4647a8a83b6972d25373274b70a018cffb8aec5
vanasnip <ivan.mamanemuno@gmail.com>
2017-11-26 17:28:47 +0000

    arguments and SPREAD

--- app.js
+++ app.js
@@ -1,18 +1,38 @@
-//Arrays
+//'arguments' and SPREAD
+//functions called comewith arguments variable
 
-var arr = [
-    1,
-    false,
-    {
-        name:'Ivano',
-        address: 'in the city'    
-    },
-    function(name){
-        var greeting = 'Hello';
-        console.log(greeting, name);
-    },
-    'whatsup'
-];
-//js arrays can be different types
-console.log(arr);
-arr[3](arr[2].name); //this is madness!!!
+//invoke function
+//execution context created
+    //variable environment
+    //this
+    //Outer environment
+    //'arguments' hold values passed to a function
+
+function greet(firstname, lastname, language){
+    // undefined coerces to false so the or alternative is selected
+    firstname = firstname || 'Barbra';
+    lastname = lastname || 'Evergreen';
+    language = language || 'en';
+    if (arguments.length === 0){
+        console.log('Missing params...');
+    } else{
+        console.log(firstname,lastname,language);
+        console.log(arguments);
+    }  
+}
+
+greet();
+greet('Jane');
+greet('Jane', 'Doe');
+greet('Jane', 'Doe', 'Anglais');
+
+
+//SPREAD
+
+function hello(first = 'jamez', last = 'bond', lang = 'Anglais', ...other){
+    console.log(first, last, lang);
+    if(other.length > 0){
+        console.log(other);
+    }
+}
+hello('van', 'man', 'japanese', 'england');
