ad7479bcff41e02065f8248b626de5b4288014d5
vanasnip <ivan.mamanemuno@gmail.com>
2017-11-26 16:49:32 +0000

    Objects, functions and this

--- app.js
+++ app.js
@@ -1,64 +1,49 @@
-//by value vs by reference
+//Objects, functions and this
+// functions invoked a new execution context is created as discussed
+//execution context created and put on execution stack
 
-// eg 
-var a = 3;//'some primitive value';
+// variable environment
+// outer environment 
+// variable called 'this'
+// 'this' can be different things
+// depending on how the function is invoked
 
-var b = a; // or pass a function
-// a new memory location is creted and a copy of a is placed there
-//this is what is called by value;
+//console.log(this); // this points to gloabal obj at this lvl in the code
 
-//in the case of ojects,
-// I still get a memory location value for that object & this includes functions,
-//very much in the way I get a memory location for my variable
+function a(){
+    console.log(this);
+    this.newVar = 'hello';
+}
+var b = function(){
+    console.log(this);
+}
+a();
+b();
+
+console.log(newVar);
+
+/////////////////////////////
 
 var c = {
-    object: 'non primitive type',
-    num: 4,
-    funcs: function(){
-        return `still an objext with name property,
-        code property
-        `;
+    name: 'the c obj',
+    log: function(){
+        var self = this;
+        self.name = 'updated c obj';
+        console.log(self);
+
+        var setName = function(newName){
+            self.name = newName;
+        }
+        setName('Updated again!');
+        console.log(self);
     }
 }
+// the object that contains the function is what this is reffering to
+// in the case of a and b the function is directly attached to the window object
+// in the case of c the function is dirctly attached to the c object.
+// whatever object the function sits on is what this is refering to...
 
-var d = c; //d is pointed to the exact location of c
-// no new object is created, 2 names pointing to same object
-// this is what we call by reference
-
-//Examples by value
-//changing a doesnt change b
-console.log(a,b); //3 3
-a = 2;
-console.log(a,b); //2 3
-
-//Examples by reference // all objects includes functions
-console.log(c.num, d.num); //4 4
-c.num = 9;
-console.log(c.num, d.num); //9 9
-//////////////////////////////////////////////////////
-var f = {greeting : 'hello'}
-var g = f;
-console.log(`f, g : ${f.greeting}, ${g.greeting} `);
-// hello, hello
-
-function changeGreeting(obj){
-    obj.greeting = 'hola';
-}
-
-changeGreeting(g);
-console.log(`f, g : ${f.greeting}, ${g.greeting} `);
-// hola, hola for both because they are the same object
-
-f = {greeting: 'gutentag'};
-console.log(`f, g : ${f.greeting}, ${g.greeting} `);
-//brand new memory space for f
-/////////////////////////////////////////////////////////
-
-console.log(`
-{} === {}: ${{} === {}}
-{} == {}: ${{} == {}}
-different objects are never equal even if they are exaclty the same
-`); //false, false
-// equivalency opperations on object are only true if its the same
-// object in memory. literally referencing the same thing, otherwise
-// it will always be false even if its an exact copy
+// functions in effect are methods of an object
+console.log(c.name);
+c.log();
+console.log(c.name);
