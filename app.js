01093a1cbcc25a88557058323bb1a88523969f57
vanasnip <ivan.mamanemuno@gmail.com>
2017-11-26 17:56:29 +0000

    no function overloading

--- app.js
+++ app.js
@@ -1,38 +1,24 @@
-//'arguments' and SPREAD
-//functions called comewith arguments variable
-
-//invoke function
-//execution context created
-    //variable environment
-    //this
-    //Outer environment
-    //'arguments' hold values passed to a function
+// no function overloading in JS
+// function of the same name that has different number os params
 
 function greet(firstname, lastname, language){
-    // undefined coerces to false so the or alternative is selected
-    firstname = firstname || 'Barbra';
-    lastname = lastname || 'Evergreen';
     language = language || 'en';
-    if (arguments.length === 0){
-        console.log('Missing params...');
-    } else{
-        console.log(firstname,lastname,language);
-        console.log(arguments);
-    }  
-}
-
-greet();
-greet('Jane');
-greet('Jane', 'Doe');
-greet('Jane', 'Doe', 'Anglais');
-
-
-//SPREAD
-
-function hello(first = 'jamez', last = 'bond', lang = 'Anglais', ...other){
-    console.log(first, last, lang);
-    if(other.length > 0){
-        console.log(other);
+    if (language === 'en') {
+        console.log('Hello', firstname, lastname);
+    }
+    if (language === 'es') {
+        console.log('Hola', firstname, lastname)
     }
 }
-hello('van', 'man', 'japanese', 'england');
+
+function greetEng(firstname, lastname){
+    greet(firstname, lastname, 'en');
+}
+function greetEsp(firstname, lastname){
+    greet(firstname, lastname, 'es');
+}
+
+greetEng('Ivano','Mams', 'en');
+greetEsp('Ignacio','Suares', 'es');
+
+// creating simpler functions 
