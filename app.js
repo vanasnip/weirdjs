76e97ccc8c9d867aa59db780c27b6e528b1dae27
vanasnip <ivan.mamanemuno@gmail.com>
2017-11-26 16:59:04 +0000

    Arrays

--- app.js
+++ app.js
@@ -1,49 +1,18 @@
-//Objects, functions and this
-// functions invoked a new execution context is created as discussed
-//execution context created and put on execution stack
+//Arrays
 
-// variable environment
-// outer environment 
-// variable called 'this'
-// 'this' can be different things
-// depending on how the function is invoked
-
-//console.log(this); // this points to gloabal obj at this lvl in the code
-
-function a(){
-    console.log(this);
-    this.newVar = 'hello';
-}
-var b = function(){
-    console.log(this);
-}
-a();
-b();
-
-console.log(newVar);
-
-/////////////////////////////
-
-var c = {
-    name: 'the c obj',
-    log: function(){
-        var self = this;
-        self.name = 'updated c obj';
-        console.log(self);
-
-        var setName = function(newName){
-            self.name = newName;
-        }
-        setName('Updated again!');
-        console.log(self);
-    }
-}
-// the object that contains the function is what this is reffering to
-// in the case of a and b the function is directly attached to the window object
-// in the case of c the function is dirctly attached to the c object.
-// whatever object the function sits on is what this is refering to...
-
-// functions in effect are methods of an object
-console.log(c.name);
-c.log();
-console.log(c.name);
+var arr = [
+    1,
+    false,
+    {
+        name:'Ivano',
+        address: 'in the city'    
+    },
+    function(name){
+        var greeting = 'Hello';
+        console.log(greeting, name);
+    },
+    'whatsup'
+];
+//js arrays can be different types
+console.log(arr);
+arr[3](arr[2].name); //this is madness!!!
