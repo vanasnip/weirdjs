ecd1bdf3fde593c4bfd3ce1b0bb0ade440f8c480
vanasnip <ivan.mamanemuno@gmail.com>
2017-12-01 06:36:08 +0000

    Closures part 2

--- app.js
+++ app.js
@@ -1,18 +1,59 @@
-//Closures
-function greet(whattosay){
-    //when this execution context if popped off
-    //the execution stack, its variables remain
-    // available and accessible as the outer
-    // environment for functions within this execution
-    // context
-    return function(name){
-        console.log(whattosay + ' ' + name);
-        // what to say is not defined in this execution
-        // context, so JS looks for it in outer environment
+//Closures part 2
+
+function buildFunctions(){
+    var arr = [];
+    for(var i = 0; i < 3; i++){
+        arr.push(
+            function(){
+                console.log(i);
+            }
+        )
     }
+    return arr;
 }
 
-greet('Hello')('Ivan');
-sayHi = greet('Hi');
-sayHi('Ivan');
+var fs = buildFunctions();
 
+// all 3 are being executed later and will return
+// value for i now and not when created
+// accessing the enclosed value of i in memory
+fs[0]();
+fs[1]();
+fs[2]();
+
+// making it work
+
+function buildFunctions2(){
+    var arr = [];
+    for(var i = 0; i < 3; i++){
+      arr.push((function(x){
+                return function(){
+                    console.log(x);
+                }
+            }(i))); // executing each time to get i
+    }
+    return arr;
+}
+var fs2 = buildFunctions2();
+
+fs2[0]();
+fs2[1]();
+fs2[2]();
+
+function buildFunctions3(){
+    var arr = [];
+    for(var i = 0; i < 3; i++){
+        let j = i; // new let keyword confines varibable to the block
+        // this will point j at a different spot in memory each time
+      arr.push(function(){
+                    console.log(j);
+                }
+            ); // executing each time to get i
+    }
+    return arr;
+}
+var fs3 = buildFunctions3();
+
+fs3[0]();
+fs3[1]();
+fs3[2]();
