b6a5fff4beb3a79dadd6a46cffa271842b04d1c7
vanasnip <ivan.mamanemuno@gmail.com>
2017-12-01 07:30:44 +0000

    Closures continues - function factories

--- app.js
+++ app.js
@@ -1,59 +1,21 @@
-//Closures part 2
-
-function buildFunctions(){
-    var arr = [];
-    for(var i = 0; i < 3; i++){
-        arr.push(
-            function(){
-                console.log(i);
-            }
-        )
+//Closures continued
+// function factories
+function makeGreeting(language){
+    return function(firstname, lastname){
+        if(language === 'en'){
+            console.log('Hello',firstname,lastname);
+        }
+        if(language === 'es'){
+            console.log('Hola',firstname,lastname);
+        }
     }
-    return arr;
 }
+//new execution context created with outer environment variables available
+var greatTheBritish = makeGreeting('en');
+//new execution context created with outer environment variables available
+var greatTheSpanish = makeGreeting('es');
 
-var fs = buildFunctions();
-
-// all 3 are being executed later and will return
-// value for i now and not when created
-// accessing the enclosed value of i in memory
-fs[0]();
-fs[1]();
-fs[2]();
-
-// making it work
-
-function buildFunctions2(){
-    var arr = [];
-    for(var i = 0; i < 3; i++){
-      arr.push((function(x){
-                return function(){
-                    console.log(x);
-                }
-            }(i))); // executing each time to get i
-    }
-    return arr;
-}
-var fs2 = buildFunctions2();
-
-fs2[0]();
-fs2[1]();
-fs2[2]();
-
-function buildFunctions3(){
-    var arr = [];
-    for(var i = 0; i < 3; i++){
-        let j = i; // new let keyword confines varibable to the block
-        // this will point j at a different spot in memory each time
-      arr.push(function(){
-                    console.log(j);
-                }
-            ); // executing each time to get i
-    }
-    return arr;
-}
-var fs3 = buildFunctions3();
-
-fs3[0]();
-fs3[1]();
-fs3[2]();
+//access outer environment in search of language variable
+greatTheBritish('Some', 'Trollope');
+//access outer environment in search of language variable
+greatTheSpanish('Rosita','Rodriges');
