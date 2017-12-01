91825b443ea9841ea99076204abd9cd73e950098
vanasnip <ivan.mamanemuno@gmail.com>
2017-12-01 04:54:51 +0000

    Closures Part 1

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
-//IIFE safe code
-var greeting = 'Hola';
-(function(global,name){
-    global.greeting = 'Hello';
-    // pass by ref so will change source of external as wee
-    console.log(greeting + ' ' + name);
-}(window, 'IFFE'));
+//Closures
+function greet(whattosay){
+    //when this execution context if popped off
+    //the execution stack, its variables remain
+    // available and accessible as the outer
+    // environment for functions within this execution
+    // context
+    return function(name){
+        console.log(whattosay + ' ' + name);
+        // what to say is not defined in this execution
+        // context, so JS looks for it in outer environment
+    }
+}
 
-console.log(greeting);
+greet('Hello')('Ivan');
+sayHi = greet('Hi');
+sayHi('Ivan');
+
