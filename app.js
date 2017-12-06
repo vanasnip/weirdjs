6130015af95833c0c9f2572c0f0dc6f182a9c62f
vanasnip <ivan.mamanemuno@gmail.com>
2017-12-06 22:04:42 +0000

    Closures & callbacks

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
-//Closures continued
-// function factories
-function makeGreeting(language){
-    return function(firstname, lastname){
-        if(language === 'en'){
-            console.log('Hello',firstname,lastname);
-        }
-        if(language === 'es'){
-            console.log('Hola',firstname,lastname);
-        }
-    }
-}
-//new execution context created with outer environment variables available
-var greatTheBritish = makeGreeting('en');
-//new execution context created with outer environment variables available
-var greatTheSpanish = makeGreeting('es');
+// Closures & callbacks
 
-//access outer environment in search of language variable
-greatTheBritish('Some', 'Trollope');
-//access outer environment in search of language variable
-greatTheSpanish('Rosita','Rodriges');
+function sayHiLater(){
+    var greeting = 'Hi';
+    setTimeout(function(){
+        console.log(greeting);
+    }, 3000);
+}
+
+sayHiLater();
+
+// $('button').click(function(){
+//     //example of use of function expressions
+//     //and first class functions
+// });
+
+//Call back function, call a function and give it a function, and when its done it calls given function
+
+function tellMeWhenDone(callback){
+    var a = 1000;
+    var b = 2000;
+
+    callback();
+}
+
+tellMeWhenDone(function(){console.log('I am done.')});
+tellMeWhenDone(function(){console.log('I am done once again.')});
