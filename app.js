23a3205b958d1a1e3df134828f8d1523be9b8393
vanasnip <ivan.mamenemuno@gmail.com>
2018-01-08 11:58:49 +0000

    Prototypal Inheritance

--- app.js
+++ app.js
@@ -1,12 +1,42 @@
-//Functional Programming Part 2
-//Play with Underscorejs, see whats going on
-//and how they use functional programming paradgm 
-var arr1 = [1,2,3,4];
+//Object-Oriented javascript and prototypal Inheritance
+//classical vs prototypal inheritance
 
-var arr6 = _.map(arr1,function(item){return item*3;})
-console.log(arr6);
-var bigLongArray = [2,3,4,5,6,7];
-var arr7 = _.filter(bigLongArray, function(item){
-    return item % 2 === 0;
-});
-console.log(arr7);
+//classical is whats currently best known and popular || how it has been done for a long time
+//a way of sharing propeties and methods || not the only method ...very verbose
+
+//prototypal inheritance
+//simple, flexible, extensible and easy to understand - not perfect
+
+//Inheritance: one object get access to the propeties and methods of another object
+
+var person = {
+    firstname: 'Default',
+    lastname: 'Default',
+    getFullName: function(){
+        var fullname = this.firstname + ' ' + this.lastname;
+        console.log(fullname);
+        return fullname;
+    }
+}
+
+var john = {
+    firstname: 'John',
+    lastname : 'Doe'
+}
+
+//Doing something you should never ever do....this is just to illustrate...its a performance problem..>>
+john.__proto__ = person;//<<< never ever do this!!!! __proto__ is named such that you'd never accidentally type this
+john.getFullName(); //o:John Doe
+
+
+var jane = {
+    firstname: 'Jane'
+}
+
+jane.__proto__ = person;//<<< never ever do this!!!!
+jane.getFullName();//o: Jane Default
+
+
+
+
+
