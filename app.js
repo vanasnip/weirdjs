1083cee2e0f24643417fb39f5304980caac8127a
vanasnip <ivan.mamenemuno@gmail.com>
2018-01-08 16:57:43 +0000

    Setting the prototype specifically in Functions Constructors

--- app.js
+++ app.js
@@ -1,40 +1,42 @@
-//Reflection & Extend
-//Reflection: An object can look at itself, listing and changing its properties and methods
-var person = {
-    firstname: 'Default',
-    lastname: 'Default',
-    getFullName : function(){
-        return this.firstname + ' ' + this.lastname;
-    }
+//Building Object, setting the prototype specifically in Functions Constructors
+//Functions Constructors, 'new and the history of JS
+
+function Person(first, last){
+    this.firstname = first;
+    this.lastname = last;
 }
 
-var john = {
-    firstname: 'John',
-    lastname: 'doe'
+//all functions get the .prototype property when used as a contructor to creat objects using new only
+Person.prototype.getFullName = function(){
+    return this.firstname + ' ' + this.lastname;
 }
 
-// don't do this EVER! For demo purposes only!!!
-john.__proto__ = person;
 
-for (var prop in john){
-    if(john.hasOwnProperty(prop)){
-        console.log(prop + ': ' + john[prop]);
-    }
-}
+//Function COnstructors:
+// a normal function that is used to create objects
 
-var jane = {
-    address: '111 Main St',
-    getFormalFullName: function(){
-        return this.lastname + ', ' + this.firstname;
-    }
-}
-
-var jim = {
-    getFirstName: function(){
-        return firstName;
-    }
-}
-
-_.extend(john, jane, jim);
-
+var john = new Person('John','Doe'); //new keyword is an opperator
+//new opperator creats a new empty object then the rest attaches to the rest
 console.log(john);
+
+var jane = new Person('Jane', 'Doe');
+console.log(jane);
+
+Person.prototype.getFormalFullName = function(){
+    return this.lastname + ', ' + this.firstname;
+}
+//different ways to construct objects
+
+//Properties are fine to sit in the function constructor, 
+//however methods which are functions but more importantly are objects
+//take up space in memory and it is very expensive computationally to have 1000 instances of objects, 
+//each of which has multiple object /slash/ methods....what to do?
+//better to have it once on the prototype and have it accessible to all instances via the prototype chian.
+
+//########################################## DANGEROUS ASSIDE ##########################################
+
+//the constructor is still just a functions so if you forget the new keyword all your 
+//objects created from the constructor will be undefined
+
+// all functions that are constructors have capital letters, this might help desern the missing new keyword
+
