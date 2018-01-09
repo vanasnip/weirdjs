6e4d9dfad164919674405b588a45b051280bc463
vanasnip <ivan.mamenemuno@gmail.com>
2018-01-09 10:45:17 +0000

    typeof, instanceof and figuring out what something is

--- app.js
+++ app.js
@@ -1,24 +1,28 @@
-//ES6 And Classes
+//typeof, instanceof and figuring out what something is
+var a = 3;
+console.log(typeof a); //number
 
-//class keyword
+var b = "Hello";
+console.log(typeof b); //string
 
-class Person { // <<<< this is still just an object 
-    constructor(firstname, lastname){
-        this.firstname = firstname;
-        this.lastname = lastname;
-    }
-    greet(){
-        return 'Hi ' + this.firstname; //this keyword to reference current execution context
-    }
+var c = {};
+console.log(typeof c); //object
+
+var d = [];
+console.log(typeof d); // weird! //object
+console.log(Object.prototype.toString.call(d)); // better! //object array
+
+function Person(name) {
+    this.name = name;
 }
-var john = new Person('John', 'Doe');
 
-class InformalPerson extends Person {
-    constructor(firstname, lastname){
-        super(firstname, lastname); // super keyword draws down passed in values
-    }
-    greet(){
-        return 'Yo ' + this.firstname; // overwrite methods.
-    }
-}
-var jane = new InformalPerson('Jane', 'Doe');
+var e = new Person('Jane');
+console.log(typeof e); //object
+console.log(e instanceof Person); // true
+
+console.log(typeof undefined); // makes sense //undefined 
+console.log(typeof null); // a bug since, like, forever... //object
+
+var z = function() { };
+console.log(typeof z);// function
+
