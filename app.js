e89c90ba676468980069f50e6985a5f18a0e01d3
vanasnip <ivan.mamenemuno@gmail.com>
2018-01-09 10:31:36 +0000

    ES6 And Classes

--- app.js
+++ app.js
@@ -1,28 +1,24 @@
-//Object.create and Pure Prototypal Inheritance
+//ES6 And Classes
 
-var person = {
-    firstname : 'Default',
-    lasatname : 'Default',
-    greet: function(){
-        return 'Hi ' + this.firstname;
+//class keyword
+
+class Person { // <<<< this is still just an object 
+    constructor(firstname, lastname){
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+    greet(){
+        return 'Hi ' + this.firstname; //this keyword to reference current execution context
     }
 }
-//very simple and straight forward way of creating objects based on other objects
-//Note: this is a newer feature || older browsers use a polyfill!
+var john = new Person('John', 'Doe');
 
-//pollyfill
-if(!Object.create){
-    Object.create = function(o){
-        if(arguments.length > 1){
-            throw new Error('Object.create inplementation only accepts the first parameter.');
-        }
-        function F(){};
-        F.prototype = o;
-        return new F();
-    };
+class InformalPerson extends Person {
+    constructor(firstname, lastname){
+        super(firstname, lastname); // super keyword draws down passed in values
+    }
+    greet(){
+        return 'Yo ' + this.firstname; // overwrite methods.
+    }
 }
-
-var john = Object.create(person);
-john.firstname = 'John';
-john.lasatname = 'Doe';
-console.log(john);
+var jane = new InformalPerson('Jane', 'Doe');
