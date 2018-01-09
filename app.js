6b69de3c006da0263c0277d7bd614b823dc2916b
vanasnip <ivan.mamenemuno@gmail.com>
2018-01-09 09:19:53 +0000

    do not use -for in- on arrays

--- app.js
+++ app.js
@@ -1,24 +1,9 @@
-//Built in function constructors
-var a = new Number(3);
-var b = new String('John');
-var c = new Date('08/01/2018');
+//BY THE WAY: doing operations with date - use momentjs instead
+//:::DANGEROUS ASSIDE:::  do not use -for in- on arrays, especially if you add custom features
 
-String.prototype.isLenghtGreaterThan = function(limit){
-    return this.length > limit;
+Array.prototype.myCustomFeature = 'cool!!';
+
+var arr = ['John', 'Jane', 'Jim'];
+for (var prop in arr){
+    console.log(prop + ': ' + arr[prop]);
 }
-console.log(b.isLenghtGreaterThan(3)); //true
-
-Number.prototype.isPositive = function(){
-    return this > 0;
-}
-//3.isPositive() wont work because JS wont convert numbers into objects automatically 
-//like the convert strings into objects automatically...
-//for this to work we must invoke the new key word...fortunately we already did with a
-console.log(a.isPositive());
-
-//DO NOT USE THESE UNLESS YOU HAVE TO!!! THEY JUST LOOK TOO MUCH LIKE PRIMITIVE TYPES WHEN THEY AREN'T, THEY ARE OBJECTS!
-var numA = 3;
-var numB = new Number(3);
-
-var test1 = (numA == numB);//o: true
-var test2 = (numA === numB); //o: false 
