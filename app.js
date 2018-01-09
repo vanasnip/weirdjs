f9aabc627e1c78eea6f47151a0aedc2f52aef80d
vanasnip <ivan.mamenemuno@gmail.com>
2018-01-09 15:12:16 +0000

    Jquery Deep Dive

--- app.js
+++ app.js
@@ -1,28 +1,4 @@
-//typeof, instanceof and figuring out what something is
-var a = 3;
-console.log(typeof a); //number
+//Jquery Deep Dive
 
-var b = "Hello";
-console.log(typeof b); //string
-
-var c = {};
-console.log(typeof c); //object
-
-var d = [];
-console.log(typeof d); // weird! //object
-console.log(Object.prototype.toString.call(d)); // better! //object array
-
-function Person(name) {
-    this.name = name;
-}
-
-var e = new Person('Jane');
-console.log(typeof e); //object
-console.log(e instanceof Person); // true
-
-console.log(typeof undefined); // makes sense //undefined 
-console.log(typeof null); // a bug since, like, forever... //object
-
-var z = function() { };
-console.log(typeof z);// function
-
+var q = $("ul.people li");
+console.log(q);
