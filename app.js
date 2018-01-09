d553a502d163d677c6d21a800af06335f7cad41c
vanasnip <ivan.mamenemuno@gmail.com>
2018-01-09 09:51:33 +0000

    Object.create and Pure Prototypal Inheritance

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
-//BY THE WAY: doing operations with date - use momentjs instead
-//:::DANGEROUS ASSIDE:::  do not use -for in- on arrays, especially if you add custom features
+//Object.create and Pure Prototypal Inheritance
 
-Array.prototype.myCustomFeature = 'cool!!';
-
-var arr = ['John', 'Jane', 'Jim'];
-for (var prop in arr){
-    console.log(prop + ': ' + arr[prop]);
+var person = {
+    firstname : 'Default',
+    lasatname : 'Default',
+    greet: function(){
+        return 'Hi ' + this.firstname;
+    }
 }
+//very simple and straight forward way of creating objects based on other objects
+//Note: this is a newer feature || older browsers use a polyfill!
+
+//pollyfill
+if(!Object.create){
+    Object.create = function(o){
+        if(arguments.length > 1){
+            throw new Error('Object.create inplementation only accepts the first parameter.');
+        }
+        function F(){};
+        F.prototype = o;
+        return new F();
+    };
+}
+
+var john = Object.create(person);
+john.firstname = 'John';
+john.lasatname = 'Doe';
+console.log(john);
